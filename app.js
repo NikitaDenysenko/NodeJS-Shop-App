c7b2d953d2957d4a8101783954fd9b2c36bd6041
Nikita Denysenko <nik2012nik92@gmail.com>
2020-10-02 11:42:32 +0300

    replaced HTML with EJS

--- app.js
+++ app.js
@@ -5,18 +5,25 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');// allows to set any values globally on our express application
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes  = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));//registers a middleware,does a req body parsing,executes next()
 // {extended: false} - should be able to parse none default features
 app.use(express.static(path.join(__dirname, 'public')));// for css to be available
 
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
+
 app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        path: null
+    })
 });
 
 app.listen(3000);

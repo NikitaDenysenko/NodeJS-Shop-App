28d50214b6912968b535ab16bdf137dac27d4c78
Nikita Denysenko <nik2012nik92@gmail.com>
2020-11-01 09:05:58 +0200

    replaced mysql with mongodb

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,10 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    req.user.createProduct({
-        title: title,
-        price: price,
-        imageUrl: imageUrl,
-        description: description,
-    }).then(result => {
+    const product = new Product(title, price, description, imageUrl, null, req.user._id);
+    product
+        .save()
+        .then(result => {
         //console.log(result);
         console.log('Created Product!');
         res.redirect('/admin/products');
@@ -33,10 +31,8 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    req.user
-        .getProducts({where: {id: prodId}})
-        .then(products => {
-            const product = products[0];
+    Product.findById(prodId)
+        .then(product => {
             if (!product) {
                 return res.redirect('/')
             }
@@ -58,24 +54,21 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDescription = req.body.description;
-    Product.findByPk(prodId)
-        .then(product => {
-            product.title = updatedTitle;
-            product.price = updatedPrice;
-            product.description = updatedDescription;
-            product.imageUrl = updatedImageUrl;
-            return  product.save();
-        })
-        .then(result => {
-            console.log('UPDATED PRODUCT');
-            res.redirect('/admin/products');
-        })
-        .catch(err => console.log(err));
+    const product = new Product(
+        updatedTitle,
+        updatedPrice,
+        updatedDescription,
+        updatedImageUrl,
+        prodId
+    );
+    product.save().then(result => {
+        console.log('UPDATED PRODUCT');
+        res.redirect('/admin/products');
+    }).catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-    req.user
-        .getProducts()
+    Product.fetchAll()
         .then(products => {
             res.render('admin/products', {
                 prods: products,
@@ -97,12 +90,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByPk(prodId)
-        .then(product => {
-            return product.destroy();
-        })
-        .then(result => {
-            console.log('DESTROYED PRODUCT');
+    Product.deleteById(prodId)
+        .then(() => {
             res.redirect('/admin/products');
         })
         .catch(err => {
